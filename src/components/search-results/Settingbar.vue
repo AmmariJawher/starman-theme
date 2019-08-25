0738c7e2b04d88c775f22f4d7ddd627ba2f3741d
AmmariJawher <jawher.ammari@tutanota.com>
2019-08-25 17:33:57 +0100

    Add setting-bar component

---
+++ src/components/search-results/Settingbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar has-background-white border-bottom">
+    <div class="container is-fluid">
+      <div id="navbarMenuHeroA" class="navbar-menu">
+        <div class="navbar-start">
+          <div class="navbar-item navbar-search is-paddingless none-border">
+          </div>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="field">
+              <label for="switchRoundedDefault">Show map</label>
+              <input id="switchRoundedDefault" type="checkbox" name="switchRoundedDefault" class="switch is-rounded" checked="checked">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'Settingbar',
+  components: {
+  },
+  data () {}
+}
+
+</script>
+
+<style lang="scss" scoped>
+.border-bottom {
+  border-top: solid #f0f0f0 1px;
+  border-bottom: solid #f0f0f0 1px;
+}
+.top-margine {
+  margin-top: 3px;
+}
+</style>
